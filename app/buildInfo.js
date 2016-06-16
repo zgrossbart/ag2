45be884b8f404bbe95bcb760cdc7d45db458614e
Zack Grossbart <zack@grossbart.com>
2016-06-16 16:02:27 -0400

    Removed all of the built artifacts and added a gitignore file so they don't get committed again.

--- app/buildInfo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var BuildInfo = (function () {
-    function BuildInfo() {
-    }
-    return BuildInfo;
-}());
-exports.BuildInfo = BuildInfo;
-//# sourceMappingURL=buildInfo.js.map
--- app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var http_1 = require('@angular/http');
-var app_component_1 = require('./app.component');
-var buildInfoService_1 = require('./buildInfoService');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS, buildInfoService_1.BuildInfoService]);
-//# sourceMappingURL=main.js.map
